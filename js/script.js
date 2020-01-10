78d89d0e0bff3c6ec137a39950907bfa7e7da71a
MiyuNoemi <59445221+MiyuNoemi@users.noreply.github.com>
2020-01-10 14:00:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 }
 
-else if(randomNumber == '2'){
+else if(randomNumber == 2){
   computerMovee = 'papier';
 }
 
-else if(randomNumber == '3'){
+else if(randomNumber == 3){
   computerMove = 'nożyce';
 }
 
@@ -27,15 +27,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
  let playerMove = 'nieznany ruch';
 
-  if(playerInput == '1'){
+  if(playerInput == 1){
      playerMove = 'kamień'; 
     }
 
- else if(playerInput == '2'){
+ else if(playerInput == 2){
     playerMove = 'papier'; 
   } 
 
- else if(playerInput == '3'){
+ else if(playerInput == 3){
     playerMove = 'nożyce'; 
   }
 
