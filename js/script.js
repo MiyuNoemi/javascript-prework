4ac0c8ab6987d583bd5988f501577be06a4e8bb4
MiyuNoemi <59445221+MiyuNoemi@users.noreply.github.com>
2020-01-10 14:02:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,15 +27,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
  let playerMove = 'nieznany ruch';
 
-  if(playerInput == 1){
+  if(playerInput == '1'){
      playerMove = 'kamień'; 
     }
 
- else if(playerInput == 2){
+ else if(playerInput == '2'){
     playerMove = 'papier'; 
   } 
 
- else if(playerInput == 3){
+ else if(playerInput == '3'){
     playerMove = 'nożyce'; 
   }
 
