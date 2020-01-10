f79726ea27dc7741a9446cb35d667f026f208bed
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-01-10 11:44:11 +0100

    Add player movement

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ else if(playerInput == '2'){
 else if(playerInput == '3'){
   playerMove = 'nożycze';
 }
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
+//gracz 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+ console.log('Gracz wpisał: ' + playerInput);
+
+ let playerMove = 'nieznany ruch';
+
+  if(playerInput == '1')
+ { playerMove = 'kamień'; }
+
+ else if(playerInput == '2')
+ { playerMove = 'papier'; } 
+
+ else if(playerInput == '3')
+ { playerMove = 'nożycze'; }
+
+  printMessage('Twój ruch to: ' + playerMove);
